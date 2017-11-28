64c48176d500fb3bf8becc6216d9cee0e536ad8f
Danni Sunne <etnichols55@gmai.com>
2017-11-28 12:51:34 -0800

    11.28.17

---
+++ src/nov28/Hauler.java
@@ -0,0 +1,4 @@
+package nov28;
+
+public class Hauler {
+}
---
+++ src/nov28/Notes.java
@@ -0,0 +1,29 @@
+package nov28;
+import java.util.ArrayList;
+
+public class Notes {
+    /*
+     * ARRAYLIST VS ARRAY
+     *      ArrayList
+     *          Pseudo-dynamic, can change size by copying array within it to a larger array containing the same values
+     *          Can be declared for a class type using <>
+     *      Array
+     *          Nondynamic, contained within ArrayList, stays one static size.
+     *
+     *      Both are modular, editing an array in a method edits it everywhere
+     */
+    public static void main(String[] args) {
+        ArrayList<String> al;
+        al = new ArrayList<>();
+
+        String[] a = new String[20];
+
+        for(String w : al) {
+            // how to: iterate thru arraylist
+            // classically:
+            // for(int i = 0; i < a.size; i++) {System.out.println(al.get(i)}
+            System.out.println(w);
+        }
+
+    }
+}
